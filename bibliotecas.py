fbfa5b96e6b3fd082bde288817ae9cedd1a74bce
camilla <camilla.schettino@engenharia.ufjf.br>
2021-07-02 02:42:29 -0300

    primeiro committ

---
+++ bibliotecas.py
@@ -0,0 +1,26 @@
+#%tensorflow_version 2.x  # this line is not required unless you are in a notebook
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+import random
+import csv
+#import xlsxwritter
+
+# Helper libraries
+import os
+import numpy as np
+from sklearn.model_selection import KFold
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.metrics as metrics
+
+#from google.colab import auth
+#import gspread
+#from oauth2client.client import GoogleCredentials
+#from gspread_dataframe import get_as_dataframe, set_with_dataframe
+#from google.colab import files
+#from itertools import combinations_with_replacement]
+
+
